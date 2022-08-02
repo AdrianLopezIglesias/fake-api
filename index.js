6e153f86c9c96cf583b90fe65dde7709fc3e208f
Adrian Iglesias <adrianlopeziglesias@outlook.com>
2022-08-02 13:33:51 -0300

    newsletter

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ app.get('/success', (req, res) => {
 app.post('/success', (req, res) => {
     res.json({ success: true })
 })
+app.post('/newsletter*', (req, res) => {
+    console.log("newsletter post requested")
+    res.json({ blogSubscription: true, newsletterSubscription: true })
+})
+app.get('/newsletter*', (req, res) => {
+    console.log("newsletter get requested")
+    res.json({ blogSubscription: true, newsletterSubscription: true })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
